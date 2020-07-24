51a9337ca1357796a63b4c0da873c80531d6e924
juliaveronica02 <juliavero2206@gmail.com>
2020-07-24 09:39:48 +0700

    add get all user and postman test

--- learn-mux-mysql/main.go
+++ learn-mux-mysql/main.go
@@ -40,41 +40,6 @@ type Result struct {
 	Message string      `json:"message"`
 }
 
-func main() {
-	// db connection.
-	// root is username, Hallo123$ is a password, @api is a database name.
-	db, err = gorm.Open("mysql", "root:Hallo123$@/api?charset=utf8&parseTime=True")
-
-	// check connection null (nil).
-	if err != nil {
-		log.Println("Connection Failed!", err)
-	} else {
-		// connection success.
-		log.Println("Connection established")
-	}
-
-	// auto migrate User table (same like nodejs we run sequelize db:migrate)
-	db.AutoMigrate(&User{})
-	// check database: use api, show tables, desc users, select *from users (it will be empty because have not create data.).
-
-	// call handle request function (route / routing).
-	handleRequests()
-}
-
-// in nodejs we call it routes.
-func handleRequests() {
-	// log.Println (console.log on javascript) to see if the routes running.
-	log.Println("Start the development server at localhost:8080!")
-
-	// package mux for library routes (in nodejs we use const Router = req(router)).
-	Router := mux.NewRouter().StrictSlash(true)
-	// homePage.
-	Router.HandleFunc("/", homePage)
-	Router.HandleFunc("/user/create", createUser).Methods("POST")
-	// port.
-	log.Fatal(http.ListenAndServe(":8080", Router))
-}
-
 // homepage function on browser (go run main.go after that open browser and write localhost:8080, result: welcome).
 func homePage(w http.ResponseWriter, r *http.Request) {
 	// Fprintf to show output in browser not on terminal.
@@ -110,4 +75,65 @@ func createUser(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	// res.body.
 	w.Write(result)
+}
+
+// get all user (show all user).
+func getAllUser(w http.ResponseWriter, r *http.Request) {
+	// make table.
+	user := []User{}
+	// get data.
+	db.Find(&user)
+	// result.
+	res := Result{Code: 200, Data: user, Message: "Success Get All User!"}
+	results, err := json.Marshal(res)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	// response.
+	w.Header().Set("Content-Type", "application/json")
+	// http status.
+	w.WriteHeader(http.StatusOK)
+	// res.body.
+	w.Write(results)
+}
+
+// in nodejs we call it routes.
+func handleRequests() {
+	// log.Println (console.log on javascript) to see if the routes running.
+	log.Println("Start the development server at localhost:8080!")
+
+	// package mux for library routes (in nodejs we use const Router = req(router)).
+	Router := mux.NewRouter().StrictSlash(true)
+	// homePage.
+	Router.HandleFunc("/", homePage)
+	// create user.
+	Router.HandleFunc("/user/create", createUser).Methods("POST")
+	// get user.
+	Router.HandleFunc("/user/show", getAllUser).Methods("GET")
+
+	// port.
+	log.Fatal(http.ListenAndServe(":8080", Router))
+}
+
+func main() {
+	// db connection.
+	// root is username, Hallo123$ is a password, @api is a database name.
+	db, err = gorm.Open("mysql", "root:Hallo123$@/api?charset=utf8&parseTime=True")
+
+	// check connection null (nil).
+	if err != nil {
+		log.Println("Connection Failed!", err)
+	} else {
+		// connection success.
+		log.Println("Connection established")
+	}
+
+	// auto migrate User table (same like nodejs we run sequelize db:migrate)
+	db.AutoMigrate(&User{})
+	// check database: use api, show tables, desc users, select *from users (it will be empty because have not create data.).
+
+	// call handle request function (route / routing).
+	handleRequests()
 }
